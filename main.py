13a77d191051cf62fc518df451ea7fd6e7169f2a
ϻartin <51332188+martin0024@users.noreply.github.com>
2024-09-18 19:30:34 -0400

    fix: improved system 2.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime, timedelta
 import re
+from loading_spinner import loading_spinner
 
-# Input file containing the HTML schedule
 FILE_IN = 'myconcordia.html'
 FILE_OUT = 'calendar.ics'
 
 class Course:
-    def __init__(self, nbr, section, component, days_times, room, instructor, start_end_date):
+    def __init__(self, name, nbr, section, component, days_times, room, instructor, start_end_date, status):
+        self.name = name
         self.nbr = nbr
         self.section = section
         self.component = component
@@ -15,15 +16,15 @@ class Course:
         self.room = room
         self.instructor = instructor
         self.start_end_date = start_end_date
+        self.status = status
 
     def __repr__(self):
-        return (f"Nbr: {self.nbr}\nSection: {self.section}\nComponent: {self.component}\n"
+        return (f"Name: {self.name}\nNbr: {self.nbr}\nSection: {self.section}\nComponent: {self.component}\n"
                 f"Days & Times: {self.days_times}\nRoom: {self.room}\nInstructor: {self.instructor}\n"
                 f"Start/End Date: {self.start_end_date}")
 
 def parse_schedule(schedule):
     pattern = r'([A-Za-z]+) (\d{1,2}:\d{2}[APM]{2}) - (\d{1,2}:\d{2}[APM]{2})'
-
     match = re.match(pattern, schedule)
 
     if match:
@@ -32,7 +33,6 @@ def parse_schedule(schedule):
         time_end = match.group(3)
 
         days = [days_str[i:i+2] for i in range(0, len(days_str), 2)]
-
         return days, time_start, time_end
     else:
         print(f"Failed to parse schedule: {schedule}")
@@ -41,13 +41,20 @@ def parse_schedule(schedule):
 def time_convert(time):
     return datetime.strptime(time, "%I:%M%p").strftime("%H%M%S")
 
+def get_first_class_day(start_date, days):
+    """Find the first occurrence of a class based on the day of the week."""
+    day_map = {'Mo': 0, 'Tu': 1, 'We': 2, 'Th': 3, 'Fr': 4}
+    first_class_day = min([day_map[day] for day in days if day in day_map])
+    days_ahead = (first_class_day - start_date.weekday() + 7) % 7
+    return start_date + timedelta(days=days_ahead)
+
 def generate_event(course):
     headers = ['BEGIN:VEVENT\n', 'END:VEVENT\n']
-    event_content = f'SUMMARY:Course {course.nbr} - {course.component}\nLOCATION:{course.room}\n'
-    description = (f'Course Number: {course.nbr}\nSection: {course.section}\nInstructor: {course.instructor}\n')
+    event_content = f'SUMMARY:Course {course.name.split(" - ")[0]} - {course.component}\nLOCATION:{course.room}\n'
+    description = (f'Course Number: {course.nbr}\nSection: {course.section}\nInstructor: {course.instructor}')
     event_content += f'DESCRIPTION:{description}\nSTATUS:CONFIRMED\n'
 
-    if(course.days_times == 'TBA'):
+    if course.days_times == 'TBA':
         return ''
 
     date_split = course.start_end_date.split(' - ')
@@ -61,18 +68,28 @@ def generate_event(course):
     start_time = time_convert(time_start)
     end_time = time_convert(time_end)
 
-    start_datetime = f"{start_date.strftime('%Y%m%d')}T{start_time}"
-    end_datetime = f"{start_date.strftime('%Y%m%d')}T{end_time}"
+    first_class_date = get_first_class_day(start_date, days)
+
+    start_datetime = f"{first_class_date.strftime('%Y%m%d')}T{start_time}"
+    end_datetime = f"{first_class_date.strftime('%Y%m%d')}T{end_time}"
 
     day_map = {'Mo': 'MO', 'Tu': 'TU', 'We': 'WE', 'Th': 'TH', 'Fr': 'FR'}
     byday = ','.join(day_map.get(day, '') for day in days if day in day_map)
     end_occurance = f'{end_date.strftime("%Y%m%d")}T000000'
 
     r_rule = f'RRULE:FREQ=WEEKLY;UNTIL={end_occurance};WKST=SU;BYDAY={byday}\n'
+    alarm = (
+        'BEGIN:VALARM\n'
+        'TRIGGER:-PT15M\n'
+        'ACTION:DISPLAY\n'
+        'DESCRIPTION:Reminder\n'
+        'END:VALARM\n'
+    )
 
     event_content += f"DTSTART;TZID=America/New_York:{start_datetime}\n"
     event_content += f"DTEND;TZID=America/New_York:{end_datetime}\n"
     event_content += r_rule
+    event_content += alarm
 
     return headers[0] + event_content + headers[1]
 
@@ -80,29 +97,43 @@ def main():
     with open(FILE_IN, 'r') as f:
         soup = BeautifulSoup(f, 'html.parser')
 
+    course_names = [name.text.strip() for name in soup.find_all('h3', {'class': 'ui-bar'})]
+
     courses = []
-    rows = soup.find_all('tr', {'id': re.compile(r'^trCLASS_MTG_VW')})
+    sections = soup.find_all('td', {'class': 'gsgroupbox'})
 
-    for row in rows:
-        cells = row.find_all('td')
-        if len(cells) >= 6:
-            cells[0].find('span').text.strip()
-            nbr = cells[0].find('span').text.strip()
-            section = cells[1].find('a').text.strip()
-            component = cells[2].find('span').text.strip()
-            days_times = cells[3].find('span').text.strip()
-            room = cells[4].find('span').text.strip()
-            instructor = cells[5].find('span').text.strip()
-            start_end_date = cells[6].find('span').text.strip()
+    for section, course_name in zip(sections, course_names):
+        rows = section.find_all('tr', {'id': re.compile(r'^trCLASS_MTG_VW')})
+        status = [status.text.strip() for status in section.find_all('span', {'class': 'PSEDITBOX_DISPONLY'})]
+        for row in rows:
+            cells = row.find_all('td')
+            if len(cells) >= 6:
+                nbr = cells[0].find('span').text.strip()
+                section = cells[1].find('a').text.strip()
+                component = cells[2].find('span').text.strip()
+                days_times = cells[3].find('span').text.strip()
+                room = cells[4].find('span').text.strip()
+                instructor = cells[5].find('span').text.strip()
+                start_end_date = cells[6].find('span').text.strip()
 
-            courses.append(Course(nbr, section, component, days_times, room, instructor, start_end_date))
+                courses.append(Course(course_name, nbr, section, component, days_times, room, instructor, start_end_date, status))
+
+    print(f'Found \033[31m{len(courses)}\033[0m courses.')
+    loading_spinner(5, message='Generating calendar file... Please wait...')
+
 
     with open(FILE_OUT, 'w+') as f:
         CALENDAR_HEADER = 'BEGIN:VCALENDAR\nVERSION:2.0\nCALSCALE:GREGORIAN\n'
         f.write(CALENDAR_HEADER)
         for course in courses:
-            f.write(generate_event(course))
+            if course.status[0] == 'Enrolled':
+                event_data = generate_event(course)
+                if event_data:
+                    f.write(event_data)
         f.write('END:VCALENDAR')
+        print(f"Calendar file generated: \033[31m{FILE_OUT}\033[0m")
+
+
 
 if __name__ == '__main__':
     main()
